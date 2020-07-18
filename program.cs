ba08ddc7a3161cfe3d45b2815c14f1c3b4239f40
andy de la cruz <drag.andy@outlook.com>
2020-07-17 21:23:19 -0500

    primeros cambios

---
+++ program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sistem1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA A TODOS SEAN BIENVENIDOS");
+
+
+        }
+
+    }
+}
